e86038bf54834488c5c598baf59e577a46e9ae24
nazihaBM <naziha1aloui@gmail.com>
2023-05-11 00:07:51 +0200

    update checkpoint axios

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import axios from 'axios';
+import React,{useState,useEffect} from 'react';
 function App() {
+  const [data,setData]=useState([])
+  useEffect(()=>{
+      axios.get('https://jsonplaceholder.typicode.com/users')
+      .then(res=>setData(res.data))
+      .catch((err)=>console.log(err));
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{data.map((e)=>(
+              <ul>
+                <li>{e.name}</li>
+              </ul>
+            )
+
+            )}</h1>
     </div>
   );
 }
